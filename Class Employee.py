87ac7969e0f807d02efb41119fbee8025035fcb5
sowmi3864 <konikisowmi@gmail.com>
2025-10-15 10:17:38 +0530

    Add files via upload

---
+++ Class Employee.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self,name):
+        self.name(self)
+    def display_name(self):
+        print(self.name)
+emp = Employee("sowmi")
+emp.display_name()
+print(emp.name)
